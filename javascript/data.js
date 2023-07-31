cabd828c15dfbf744486664396012d83a0e7d79c
Malekchairat <malek.chairat@esprit.tn>
2023-07-31 09:57:53 +0100

    first commit

---
+++ javascript/data.js
@@ -0,0 +1,185 @@
+// Declare an array
+import grill from "../Restaurants/American/Not vegan/bbq grill.jpg"
+ 
+  const Restaurant_american = [
+    {
+      id: 'bbq_grill',
+      Image: grill,
+      
+      
+    },
+    {
+        id: 'burger heaven',
+        Image: burger heaven, 
+    },
+    {
+        id: 'seafood habor',
+        Image: seafood habor,
+    },
+   
+  ];
+
+  const Restaurant_american_vegan = [
+    {
+      id: 'crossroads',
+      Image: crossroads; 
+      
+      
+    },
+    {
+        id: 'greenleaf cafe',
+        Image: greenleaf cafe; 
+    },
+    {
+        id: 'vedge',
+        Image: vedge; 
+    },
+   
+  ];
+
+  const Restaurant_asian= [
+    {
+      id: 'golden dragon',
+      Image: golden dragon; 
+      
+      
+    },
+    {
+        id: 'Sakura sushi bar',
+        Image: Sakura sushi bar; 
+    },
+    {
+        id: 'Tandouri',
+        Image: Tandouri; 
+    },
+   
+  ];
+
+  const Restaurant_asian_vegan= [
+    {
+      id: 'curryleaf',
+      Image: curryleaf; 
+      
+      
+    },
+    {
+        id: 'lotus blossom',
+        Image: lotus blossom; 
+    },
+    {
+        id: 'Zen garden',
+        Image: Zen garden; 
+    },
+   
+  ];
+
+
+  const Restaurant_french= [
+    {
+      id: 'le bonaparte',
+      Image: le bonaparte; 
+      
+      
+    },
+    {
+        id: 'steak frites',
+        Image: steak frites; 
+    },
+    {
+        id: 'le petit bistrot',
+        Image: le petit bistrot; 
+    },
+   
+  ];
+
+  const Restaurant_french_vegan= [
+    {
+      id: 'la cuisine vegan',
+      Image: la cuisine vegan; 
+      
+      
+    },
+    {
+        id: 'le jardin',
+        Image: le jardin; 
+    },
+    {
+        id: 'le petit potager',
+        Image: le petit potager; 
+    },
+   
+  ];
+
+
+  const Restaurant_mexican= [
+    {
+      id: 'Aztera',
+      Image: Aztera; 
+      
+      
+    },
+    {
+        id: 'panchos',
+        Image: panchos; 
+    },
+    {
+        id: 'taquera el ranchero',
+        Image: taquera el ranchero; 
+    },
+   
+  ];
+
+  const Restaurant_mexican_vegan= [
+    {
+      id: 'chiptole',
+      Image: chiptole; 
+      
+      
+    },
+    {
+        id: 'Moes',
+        Image: Moes; 
+    },
+    {
+        id: 'taco bell',
+        Image: taco bell; 
+    },
+   
+  ];
+
+  const Restaurant_italian= [
+    {
+      id: 'Bella italia',
+      Image: Bella italia; 
+      
+      
+    },
+    {
+        id: 'la piazza',
+        Image: la piazza; 
+    },
+    {
+        id: 'mama mia',
+        Image: mama mia; 
+    },
+   
+  ];
+
+  const Restaurant_italian_vegan= [
+    {
+      id: 'green olive',
+      Image: green olive; 
+      
+      
+    },
+    {
+        id: 'la cuicina verde',
+        Image: la cuicina verde; 
+    },
+    {
+        id: 'veganoteca',
+        Image: veganoteca; 
+    },
+   
+  ];
+
